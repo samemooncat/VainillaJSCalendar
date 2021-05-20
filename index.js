48c91574982d4f5abed9a17a72a173fd6a4aa187
samemooncat <cgarces711@gmail.com>
2021-05-20 11:45:22 -0500

    Initial design changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const actualYear = 2021
-const locale = 'ko'
+const locale = 'en'
 
 const weekDays = [...Array(7).keys()]
 const months = [...Array(12).keys()]
@@ -12,25 +12,25 @@ const weekDaysNames = weekDays.map((weekDayIndex) => {
     return weekDayName
 })
 
-const renderedWeekDays = weekDaysNames.map((weekDayName) => `<li class="day-name">${weekDayName}</li>`).join('')
+const renderedWeekDays = weekDaysNames.map((weekDayName) => `<li class="day-name">${weekDayName.charAt(0)}</li>`).join('')
 
 const calendar = months.map((monthKey) => {
     const monthName = intl.format(new Date(actualYear, monthKey))
     const nextMonthIndex = monthKey + 1
     const daysOfMonth = new Date(actualYear, nextMonthIndex, 0).getDate()
     const startsOn = new Date(actualYear, monthKey, 1).getDay()
-    return { monthName, daysOfMonth, startsOn }
+    return { monthName, daysOfMonth, startsOn, nextMonthIndex }
 })
 
-const html = calendar.map(({ daysOfMonth, monthName, startsOn }) => {
-    const title = `<h1>${monthName} ${actualYear}</h1>`
+const html = calendar.map(({ daysOfMonth, monthName, startsOn, nextMonthIndex }) => {
+    const title = `<div class="month-title"> <h4>${monthName}</h4><h4> ${nextMonthIndex}</h4></div>`
     const days = [...Array(daysOfMonth).keys()]
     const firstDayAttribute = `class='first-day' style='--first-day-start: ${startsOn}'`
     const renderDays = days.map((day, index) =>
         `<li ${index === 0 ? firstDayAttribute : ''}>${day + 1}</li>`
     ).join('')
 
-    return `${title}<ol>${renderedWeekDays}${renderDays}</ol> `
+    return `<div>${title}<ol>${renderedWeekDays}${renderDays}</ol></div>`
 }).join('')
 
-document.querySelector('div').innerHTML = html
+document.getElementById('container-calendar').innerHTML = html
